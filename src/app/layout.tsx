404c6b53273956fa0fc0cd8a87447d86c753c44c
Moeen-Islam <moeenislam809@gmail.com>
2026-07-22 11:37:38 +0600

    Microsoft Clarity added for Tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,17 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xqa6t7q435");
+            `
+          }}
+        />
         {pixelId && (
           <>
             <script
